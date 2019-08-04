c0270e0a4a50b85e8ad85e0d30b1a02a2047f35c
Bernardo <behornerl@gmail.com>
2019-08-04 08:11:54 -0300

    Agora é possível se esconder

--- Game Jam + 2019/Game Jam + 2019/Assets/Scripts/Esconderijo.cs
+++ Game Jam + 2019/Game Jam + 2019/Assets/Scripts/Esconderijo.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class Esconderijo : MonoBehaviour
 {
+    private Vector3 posicaoAnterior;
+    private bool escondido;
+    private GameObject jogador;
+
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if(collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
         {
+            jogador = collision.gameObject;
+            jogador.SetActive(false);
 
+            posicaoAnterior = collision.transform.position;
+            jogador.transform.position = transform.position;
+            jogador.GetComponent<ControleJogador>().escondido = true;
+
+            StartCoroutine(GerenciaJogadorEscondido());
         }
     }
+
+    // Usado para poder desabilitar o gameObject do jogador 
+    IEnumerator GerenciaJogadorEscondido()
+    {
+        yield return new WaitForSeconds(1f);
+
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                jogador.transform.position = posicaoAnterior;
+                jogador.GetComponent<ControleJogador>().escondido = false;
+                jogador.SetActive(true);
+                gameObject.SetActive(false);
+                gameObject.SetActive(true);
+                StopCoroutine(GerenciaJogadorEscondido());
+            }
+            yield return null;
+        }
+    }
+
+
 }
